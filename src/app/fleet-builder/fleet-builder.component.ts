8e0234fe78208b95c6f8e8d59c4103ad35f7c953
Phil Armstrong <philip.armstrong@autotrader.co.uk>
2017-05-16 15:50:47 +0100

    Stuf

--- src/app/fleet-builder/fleet-builder.component.ts
+++ src/app/fleet-builder/fleet-builder.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Fleet} from './Fleet';
 import {Ship} from '../models/Ship';
 import {ChosenShip} from 'app/chosen-ship/chosen-ship';
@@ -8,13 +8,17 @@ import {ChosenShip} from 'app/chosen-ship/chosen-ship';
   templateUrl: './fleet-builder.component.html',
   styleUrls: ['./fleet-builder.component.css']
 })
-export class FleetBuilderComponent implements OnInit {
+export class FleetBuilderComponent implements OnInit, OnChanges {
   public fleet: Fleet = new Fleet();
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 
   public addShip(ship: Ship) {
